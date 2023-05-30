1260b2e154b3d743de15fde23cec33bfa871656e
Ekrem <yusufekremunlu@gmail.com>
2023-05-30 19:32:52 +0300

    Register page ui completed

---
+++ app/src/main/java/com/yusufekremunlu/easyway/ui/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.yusufekremunlu.easyway.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yusufekremunlu.easyway.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/yusufekremunlu/easyway/ui/login/LoginFragment.java
+++ app/src/main/java/com/yusufekremunlu/easyway/ui/login/LoginFragment.java
@@ -7,44 +7,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import com.yusufekremunlu.easyway.R;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 public class LoginFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public LoginFragment() {
-        // Required empty public constructor
-    }
-
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        // Gerekli boş yapıcı metod
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+        View goToSignUpPage = rootView.findViewById(R.id.goToSignUp);
+
+        goToSignUpPage.setOnClickListener(v -> navigateToRegisterFragment());
+
+        return rootView;
+    }
+
+    private void navigateToRegisterFragment() {
+        Navigation.findNavController(requireView()).navigate(R.id.action_loginFragment_to_registerFragment);
     }
 }
