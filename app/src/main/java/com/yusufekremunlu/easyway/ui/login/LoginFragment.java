1fba654c8d6f10546d1533b47182b01b0b54d11d
Ekrem <yusufekremunlu@gmail.com>
2023-05-30 18:09:37 +0300

    login page ui created

--- app/src/main/java/com/yusufekremunlu/easyway/ui/login/LoginFragment.java
+++ app/src/main/java/com/yusufekremunlu/easyway/ui/login/LoginFragment.java
@@ -1,20 +1,12 @@
 package com.yusufekremunlu.easyway.ui.login;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.yusufekremunlu.easyway.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LoginFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -30,14 +22,6 @@ public class LoginFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static LoginFragment newInstance(String param1, String param2) {
         LoginFragment fragment = new LoginFragment();
